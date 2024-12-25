056c0fa8d78218ebcee8be11f68d20d8ec098e9e
Fardeen Khan <159772589+Fardeennnnnnnnnnkhan@users.noreply.github.com>
2024-12-25 23:41:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx'
 
-export const server = 'http://localhost:3000'
+export const server = 'https://easelearn-server-1.onrender.com'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
